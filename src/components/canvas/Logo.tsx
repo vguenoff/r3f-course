9be6df66d22ba622e0ce4616028ed9359472a0eb
vguenoff <vguenoff@gmail.com>
2022-12-14 00:21:04 +0200

    edit logo example

--- src/components/canvas/Logo.tsx
+++ src/components/canvas/Logo.tsx
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import { EllipseCurve } from 'three'
 import { useMemo, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
 import { useFrame } from '@react-three/fiber'
@@ -7,10 +7,10 @@ import { Line, useCursor } from '@react-three/drei'
 export default function Logo({ route, ...props }) {
   const router = useRouter()
   const mesh = useRef(null)
-  const [hovered, hover] = useState(false)
-  const points = useMemo(() => new THREE.EllipseCurve(0, 0, 3, 1.15, 0, 2 * Math.PI, false, 0).getPoints(100), [])
+  const [hover, setHover] = useState(false)
+  const points = useMemo(() => new EllipseCurve(0, 0, 3, 1.15, 0, 2 * Math.PI, false, 0).getPoints(100), [])
 
-  useCursor(hovered)
+  useCursor(hover)
   useFrame((state, delta) => {
     const t = state.clock.getElapsedTime()
     mesh.current.rotation.y = Math.sin(t) * (Math.PI / 8)
@@ -26,9 +26,12 @@ export default function Logo({ route, ...props }) {
       <Line worldUnits points={points} color='#1fb2f5' lineWidth={0.15} rotation={[0, 0, 1]} />
       {/* @ts-ignore */}
       <Line worldUnits points={points} color='#1fb2f5' lineWidth={0.15} rotation={[0, 0, -1]} />
-      <mesh onClick={() => router.push(route)} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)}>
+      <mesh
+        onClick={() => router.push(route)}
+        onPointerOver={() => setHover(true)}
+        onPointerOut={() => setHover(false)}>
         <sphereGeometry args={[0.55, 64, 64]} />
-        <meshPhysicalMaterial roughness={0} color={hovered ? 'hotpink' : '#1fb2f5'} />
+        <meshPhysicalMaterial roughness={0} color={hover ? 'hotpink' : '#1fb2f5'} />
       </mesh>
     </group>
   )
